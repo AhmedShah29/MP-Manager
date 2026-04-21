55ba889f8434ccfc1525d3fdd98c7140105c1093
Ahmed Shah <ahmedshawgi766@gmail.com>
2026-04-21 16:08:45 +0300

    Package Files

--- setup.py
+++ setup.py
@@ -23,15 +23,15 @@ setup(
         "Topic :: Games/Entertainment",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         "requests>=2.25.0",
+        "platformdirs>=2.0.0",
     ],
     entry_points={
         "console_scripts": [
